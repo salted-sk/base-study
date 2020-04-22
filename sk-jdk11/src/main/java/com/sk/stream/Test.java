34c62f7a8e21c7b1c10477f4bcb7a554a6c9a5f6
sk <731109627@qq.com>
2020-04-22 16:26:44 +0800

    新增study

---
+++ sk-jdk11/src/main/java/com/sk/stream/Test.java
@@ -0,0 +1,19 @@
+package com.sk.stream;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+/**
+ * TODO
+ *
+ * @author zhangqiao
+ * @since 2020/4/22 14:26
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Stream.of("a","b","c","","e","f").dropWhile(s-> !s.isEmpty())
+                .forEach(System.out::print);
+
+    }
+}
